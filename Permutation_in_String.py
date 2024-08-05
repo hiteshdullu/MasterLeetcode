4bb36dfd8fd63ecce0939fe217f25bad9ecdb49f
Hitesh Dullu <43543774+hiteshdullu@users.noreply.github.com>
2024-08-06 00:02:50 +0530

    Update Permutation_in_String.py

--- Permutation_in_String.py
+++ Permutation_in_String.py
@@ -2,28 +2,38 @@ class Solution:
     def checkInclusion(self, s1: str, s2: str) -> bool:
         s1_len = len(s1)
         s2_len = len(s2)
-
         if s2_len < s1_len:
             return False
-        
-        s1_wc = [0]*26
-        s2_wc = [0]*26
+        s1_freq = [0]*26
+        s2_freq = [0]*26
 
-        for i in s1:
-            s1_wc[ord(i)-ord('a')]+=1
-        
         for i in range(s1_len):
-            s2_wc[ord(s2[i])-ord('a')]+=1
-
+            s1_freq[ord(s1[i])-ord('a')] += 1
+            s2_freq[ord(s2[i])-ord('a')] += 1
+        
+        matches = 0
+        for i in range(26):
+            if s1_freq[i] == s2_freq[i]:
+                matches+=1
+        
         l = 0
         r = s1_len
-        while r < s2_len:
-            if s1_wc == s2_wc:
-                return True
-            s2_wc[ord(s2[l])-ord('a')]-=1
-            s2_wc[ord(s2[r])-ord('a')]+=1
-            l+=1
+
+        while(matches != 26 and r < s2_len):
+            index = ord(s2[r])-ord('a')
+            s2_freq[index]+=1
+            if s2_freq[index] == s1_freq[index]:
+                matches+=1
+            elif s2_freq[index] - 1 == s1_freq[index]:
+                matches-=1
             r+=1
-        if s1_wc == s2_wc:
-                return True
+            index = ord(s2[l])-ord('a')
+            s2_freq[index]-=1
+            if s2_freq[index] == s1_freq[index]:
+                matches+=1
+            elif s2_freq[index] + 1 == s1_freq[index]:
+                matches-=1
+            l+=1
+        if matches == 26:
+            return True
         return False
