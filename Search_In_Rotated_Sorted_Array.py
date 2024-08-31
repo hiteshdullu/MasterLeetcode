d3fbb9d5c6de92f152bb9b1868af715f97187aed
Hitesh Dullu <43543774+hiteshdullu@users.noreply.github.com>
2024-08-31 11:52:24 +0530

    Create Search_In_Rotated_Sorted_Array.py

---
+++ Search_In_Rotated_Sorted_Array.py
@@ -0,0 +1,19 @@
+class Solution:
+    def search(self, nums: List[int], target: int) -> int:
+        i = 0
+        j = len(nums)-1
+        while i <= j:
+            mid = (i+j)//2
+            if nums[mid] == target:
+                return mid
+            if nums[i] <= nums[mid]:
+                if nums[i] <= target and nums[mid] > target:
+                    j = mid-1
+                else:
+                    i = mid+1
+            else:
+                if nums[mid] < target and nums[j] >= target:
+                    i = mid+1
+                else:
+                    j = mid-1
+        return -1
